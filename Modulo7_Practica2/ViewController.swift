b60595066463347100e4e6e733f5f6fb104317ec
Ricardo191998 <lib.pod.apa@hotmail.com>
2023-10-10 14:19:29 -0600

    Initial Commit

---
+++ Modulo7_Practica2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Modulo7_Practica2
+//
+//  Created by Ricardo Rosales Romero on 10/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
